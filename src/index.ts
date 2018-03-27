bfc2b8adf25ef05ed56d3e2aaf4f5794206c51db
FStriewski <fstriewski@outlook.com>
2018-03-27 19:20:55 +0200

    Register controller

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,16 @@
 import 'reflect-metadata'
 import { createKoaServer, Action, BadRequestError, useKoaServer } from "routing-controllers"
 import setupDb from './db'
-//import     from './users/controller'
-//import     from './products/controller'
+import UserController from './users/controller'
+import ProductController from './products/controller'
 import * as Koa from 'koa'
 
 const port = process.env.PORT || 4008
 
 const app = createKoaServer({
     controllers: [
-
+        UserController,
+        ProductController
 
     ]
 })
